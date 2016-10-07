445fd0b6222c301dc001195a0ddddcedfc6472c2
Andrew Piper <piper.andrew@spcollege.edu>
2016-10-07 10:00:53 -0400

    Updated build.

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+var build = require('./build');
+
+gulp.task("default", function () {
+    return build(__dirname + '/node_modules/swagger-client/', __dirname + "/dist");
+});
